46a2ba75fbd87d22e98408b675522f378eb96940
hb <hb@aroma-project.org>
2022-02-12 11:23:21 -0800

    CLEANUP: Moved wait_for_file() to incl/files.sh, which now depends on incl/asserts.sh

--- bin/incl/files.sh
+++ bin/incl/files.sh
@@ -1,6 +1,8 @@
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# NAVIGATION
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+pwd=${BASH_SOURCE%/*}
+
+# shellcheck source=incl/asserts.sh
+source "${pwd}"/asserts.sh
+
 function change_dir {
     local opwd
     opwd=${PWD}
@@ -23,4 +25,20 @@ function equal_dirs {
     a=$(readlink -f "$1")
     b=$(readlink -f "$2")
     [[ "${a}" == "${b}" ]]
+}
+
+function wait_for_file {
+    local file
+    local maxseconds
+    local delay
+    
+    file=${1:?}
+    maxseconds=${2:-300}
+    delay=${3:-1.0}
+    
+    t0=${SECONDS}
+    until [[ -f "${file}" && $((SECONDS - t0)) -lt "${maxseconds}" ]]; do
+        sleep "${delay}"
+    done
+    [[ -f "${file}" ]] || error "Waited for file ${file}, but gave up after ${maxseconds} seconds"
 }
