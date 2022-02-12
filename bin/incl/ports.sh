69899422b811f264fdef6a62f0362a463978d69e
Henrik Bengtsson <hb@aroma-project.org>
2022-02-12 10:07:30 -0800

    Add very-first prototype of 'freeport' tool

--- bin/incl/ports.sh
+++ bin/incl/ports.sh
@@ -26,6 +26,8 @@ function free_random_port {
     skip=${3:-0}
     min=${4:-1024}
     max=${5:-65535}
+
+    mdebug "free_random_port(seed=${seed}, algorithm='${algorithm}', skip=${skip}, min=${min}, max=${max}) ..."
     
     [[ -n "${seed}" ]] && assert_integer "${seed}"
     assert_port "${min}"
@@ -33,30 +35,38 @@ function free_random_port {
     assert_executable python
 
     if [[ "${algorithm}" == "random" ]]; then
+        mdebug "- Algorithm '${algorithm}':"
         if [[ -z ${seed} && ${min} -eq 1024 && ${max} -eq 65535 ]]; then
+            mdebug "- Ask system for a random port by binding to port 0"
             # get unused socket per https://unix.stackexchange.com/a/132524
             # tiny race condition between the Python and launching the rserver
-            port=$(python -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')
+            code="import socket; s=socket.socket(); s.bind(('', 0)); print(s.getsockname()[1]); s.close()"
         elif [[ -z ${seed} ]]; then
             code="import random\nimport socket\ns=socket.socket(socket.AF_INET, socket.SOCK_STREAM)\nfor ii in range(0,1000):\n  port=random.randrange(${min},${max})\n  if s.connect_ex((\"\", port)) != 0: break\nprint(port)"
         else
-            code="import random\nimport socket\ns=socket.socket(socket.AF_INET, socket.SOCK_STREAM)\nrandom.seed(${seed})\nfor ii in range(0,1000):\n  port=random.randrange(${min},${max})\n  if ii >= ${skip} and s.connect_ex((\"\", port)) != 0: break\nprint(port)"
+            code="import random\nimport socket\ns=socket.socket(socket.AF_INET, socket.SOCK_STREAM)\nrandom.seed(${seed})\nfor ii in range(0,1000):\n  port=random.randrange(${min},${max})\n  if ii >= ${skip} and s.connect_ex(('', port)) != 0: break\nprint(port)"
         fi
     elif [[ "${algorithm}" == "increasing" || "${algorithm}" == "decreasing" ]]; then
+        mdebug "- Algorithm '${algorithm}':"
         range=${min}+${skip},${max},+1
         if [[ "${algorithm}" == "decreasing" ]]; then
             range=${max}-${skip},${min},-1
         fi
         mdebug "${range}"
-        code="import socket\ns=socket.socket(socket.AF_INET, socket.SOCK_STREAM)\nfor port in range(${range}):\n  if s.connect_ex((\"\", port)) != 0: break\nprint(port)\n"
+        code="import socket\ns=socket.socket(socket.AF_INET, socket.SOCK_STREAM)\nfor port in range(${range}):\n  if s.connect_ex(('', port)) != 0: break\nprint(port)\n"
         mdebug "${code}"
     else
         error "Unknown value on --algorithm=\"${algorithm}\""
     fi
 
+    [[ -z ${code} ]] && error "[INTERNAL]: No Python code"
+    
     #shellcheck disable=SC2059
     port=$(printf "${code}" | python -)
     assert_port "${port}"
+
+    mdebug "free_random_port(seed=${seed}, algorithm=${algorithm}, skip=${skip}, min=${min}, max=${max}) ... done"
+    
     echo "${port}"
 }    
 
