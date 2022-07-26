da782c068db900e13f2414d50275b73e7feab3ae
Henrik Bengtsson <hb@aroma-project.org>
2022-07-26 20:48:32 +0200

    Still needs assert_port() and assert_port_free()

---
+++ bin/incl/ports.sh
@@ -0,0 +1,23 @@
+# -------------------------------------------------------------------------
+# PORTS
+#
+# Requirements:
+# * asserts.sh
+# -------------------------------------------------------------------------
+pwd=${BASH_SOURCE%/*}
+
+# shellcheck source=incl/asserts.sh
+source "${pwd}"/asserts.sh
+
+function assert_port {
+    [[ -z $1 ]] && error "Port must not be empty"
+    assert_integer "$1"
+    if [[ $1 -lt 1 ]] || [[ $1 -gt 65535 ]]; then
+        error "Port out of range [1,65535]: $1"
+    fi
+}
+
+function assert_port_free {
+    assert_port "${1}"
+    p4m_can_port_be_opened "$1" || error "Port is already in use on $(hostname): ${1}"
+}
